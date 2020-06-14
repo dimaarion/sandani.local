a3031caa7340e968bb681218fac6ddebe9d365e4
dimaarion <dimaarion@gmail.com>
2020-06-14 22:38:49 +0300

    dimaarion/sandani.local.git

--- api/admin/test.php
+++ api/admin/test.php
@@ -3,5 +3,9 @@ spl_autoload_register(function ($className) {
     require '../classes/' . $className . '.php';
 });
 
-$insert = new DInsert();
-print_r($insert->DQuery()) ;
+$insert = new DSelect('articles, menu, menu_articles');
+print_r($insert->queryRowJ('articles.art_id = menu_articles.articles AND menu.menu_id = menu_articles.menu AND  menu.menu_id = ?',1)) ;
+
+?>
+
+<a href="/api/admin/test.php?names=ыыыыыыыыыы&alias=ыыыыыыыыыыы&content=ыыыыыыыыы">ok</a>
