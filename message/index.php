62e3ab0f541642d94a52f9dfb0167fc84f165533
Dmitriy <48031182+dimaarion@users.noreply.github.com>
2020-06-26 00:38:13 +0300

    dimaarion/sandani.local.git

--- message/index.php
+++ message/index.php
@@ -13,58 +13,62 @@
 </head>
 
 <body>
-    <div class="container ">
+    <?php print_r($_REQUEST); ?>
+    <div class="container">
         <h2>Напишите нам сообщение</h2>
         <p>Если у Вас есть вопросы или Вы желаете оставить заявку на замер окон, заполните форму, приведенную ниже.</p>
 
         <p>* Все поля ОБЯЗАТЕЛЬНЫ для заполнения!</p>
-        <form class="needs-validation" action="/message" method="POST">
-            <div class="form">
-                <div class="col-md-4 mb-3">
-                    
-                    <label for="username">Имя</label>
-                    <input name="username" type="text" class="form-control" id="username" placeholder="Имя" >
-                    <div class="invalid-feedback">
-                        Введите свое имя.
-                    </div>
-                </div>
-                <div class="col-md-4 mb-3">
-                    <label for="validationCustomUsername">Электронная почта</label>
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="inputGroupPrepend">@</span>
-                        </div>
-                        <input name="email-message" type="text" class="form-control" id="validationCustomUsername" placeholder="Электронная почта" aria-describedby="inputGroupPrepend">
-                        <div class="invalid-feedback">
-                            Введите свой почтовый адрес.
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="form">
-                <div class="col-md-6 mb-3">
-                    <label for="validationCustom03">Тема</label>
-                    <input name="subject" type="text" class="form-control" id="validationCustom03" placeholder="Тема сообщения">
-                    <div class="invalid-feedback">
-                        Тема.
-                    </div>
-                </div>
-                <div class="col-sm mb-3">
+        <div class="row">
+            <div class="col-sm-3"></div>
+            <div class="col-sm">
+                <form class="needs-validation" action="/message" method="get" id="form_message">
+                    <div class="form">
+                        <div class="col-md mb-3">
 
-                    <label for="my-input">Сообщение</label>
-                    <textarea name="message" id="my-input" class="form-control" rows="3" placeholder="Сообщение"></textarea>
-                    <div class="invalid-feedback">
-                        Напишите сообщение.
+                            <label for="username">Ф. И. О. <span id="namespan"></span></label>
+                            <input name="username" type="text" class="form-control" id="username" placeholder="Ф. И. О.">
+
+                        </div>
+                        <div class="col-md mb-3">
+                            <label for="usertel">Номер телефона <span id="telspan"></span></label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroupPrepend">&#9742</span>
+                                </div>
+                                <input name="usertel" type="text" class="form-control" id="usertel" placeholder="0 (373) 777 7-77-77" aria-describedby="inputGroupPrepend">
+
+                            </div>
+                        </div>
+                        <div class="col-md mb-3">
+                            <label for="usermail">Электронная почта <span id="mailspan"></span></label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroupPrepend">@</span>
+                                </div>
+                                <input name="usermail" type="text" class="form-control" id="usermail" placeholder="Электронная почта" aria-describedby="inputGroupPrepend">
+
+                            </div>
+                        </div>
+                        <div class="col-md mb-3">
+                            <label for="theme">Тема</label>
+                            <input name="theme" type="text" class="form-control" id="theme" placeholder="Тема сообщения">
+                            <div class="invalid-feedback">
+                                Тема.
+                            </div>
+                        </div>
+                        <div class="col-sm mb-3">
+                            <label for="message">Сообщение</label>
+                            <textarea name="message" id="message" class="form-control" rows="3" placeholder="Сообщение"></textarea>
+
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    
-                    <button name="message_send" value="Отправить сообщение" class="btn btn-primary" type="submit">Отправить сообщение</button>
-        </form>
-    </div>
-    <div id="imagies" class="container text-center"></div>
-    </div>
-    </div>
+
+                    <button name="message_send" id="submitd" value="Отправить сообщение" class="btn btn-primary ml-3" type="submit">Отправить сообщение</button>
+                </form>
+            </div>
+            <div class="col-sm-3"></div>
+        </div>
     </div>
     <script src="/js/message.js"></script>
 </body>
